2bc5f7ffe4c5ff4db8c1c247735df9908bdbbdba
Michael Mendioola <mendiola.67@osu.edu>
2017-11-04 19:54:03 -0400

    min_max array

--- js/geo.js
+++ js/geo.js
@@ -1,17 +1,43 @@
 var data = [];
+var x, y, lat, long;
+function inputLat(){
+  x = +document.getElementById('lat');
+  lat = x.elements= "lat";
+}
+
+function inputLong(){
+  y = +document.getElementById('long');
+  long = y.elements = "long";
+}
 d3.text("data/location.csv", function(unParsed) {
   data = d3.csvParse(unParsed);
+  data.forEach(function(d){
+    d.min_lat = +d.min_lat;
+    d.max_lat = +d.max_lat;
+    d.min_long = +d.min_long;
+    d.max_long = +d.max_long;
+  })
   function logNeighbourhoods(element, index, array){
     console.log(array[index]);
   }
-  console.log("neighbourhoods:");
-  data.forEach(logNeighbourhoods);
 });
 
-function inputLat{
-var x = document.getElementById('lat');
+var s = function arraySearch(arr,lat, long) {
+    for (var i=0; i<arr.length; i++){
+        if (lat >= arr[i].min_lat && lat <=arr[i].max_lat
+          && long >= arr[i].min_long && long <= arr[i].max_long){
+          var loc = arr[i].neighbourhood;
+        }
+    }
+    return loc
+  }
+
+function varCheck(x, y){
+  var s;
+  if (x>y){
+    s = "test";
+  }
+  console.log(s);
 }
 
-function inputLong{
-var x = document.getElementById('long');
-}
+console.log(s(data, lat, long));
