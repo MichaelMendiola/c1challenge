138da743ebfb058696eafa6e064033cb6f5c5b48
Michael Mendioola <mendiola.67@osu.edu>
2017-11-04 16:55:35 -0400

    input fields

--- js/geo.js
+++ js/geo.js
@@ -1,3 +1,17 @@
-d3.csv("/data/alamo.csv").get(function (error, data) {
-  console.log(data);
-})
+var data = [];
+d3.text("data/location.csv", function(unParsed) {
+  data = d3.csvParse(unParsed);
+  function logNeighbourhoods(element, index, array){
+    console.log(array[index]);
+  }
+  console.log("neighbourhoods:");
+  data.forEach(logNeighbourhoods);
+});
+
+function inputLat{
+var x = document.getElementById('lat');
+}
+
+function inputLong{
+var x = document.getElementById('long');
+}
