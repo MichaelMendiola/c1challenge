ecc0db0a495c3a22d20467ccc390d367608c97b3
Michael Mendioola <mendiola.67@osu.edu>
2017-11-10 14:11:43 -0500

    geo page

--- js/geo.js
+++ js/geo.js
@@ -1,43 +1,80 @@
 var data = [];
-var x, y, lat, long;
-function inputLat(){
-  x = +document.getElementById('lat');
-  lat = x.elements= "lat";
-}
-
-function inputLong(){
-  y = +document.getElementById('long');
-  long = y.elements = "long";
-}
+var prop =[];
+var nbh = [];
+var nbrh;
 d3.text("data/location.csv", function(unParsed) {
   data = d3.csvParse(unParsed);
   data.forEach(function(d){
+    d.latitude = +d.latitude;
+    d.longitude = +d.longitude;
+  })
+});
+d3.text("data/prop_type.csv", function(unparsed) {
+  prop = d3.csvParse(unparsed);
+  prop.forEach(function(d, i){
+    type: d[0];
+    number: +d[1];
+  });
+});
+d3.text("data/neighbourhood.csv", function(unParsed) {
+  nbh = d3.csvParse(unParsed);
+  nbh.forEach(function(d){
     d.min_lat = +d.min_lat;
     d.max_lat = +d.max_lat;
     d.min_long = +d.min_long;
     d.max_long = +d.max_long;
   })
-  function logNeighbourhoods(element, index, array){
-    console.log(array[index]);
-  }
 });
 
-var s = function arraySearch(arr,lat, long) {
-    for (var i=0; i<arr.length; i++){
-        if (lat >= arr[i].min_lat && lat <=arr[i].max_lat
-          && long >= arr[i].min_long && long <= arr[i].max_long){
-          var loc = arr[i].neighbourhood;
+  function arraySearch() {
+    var x = parseFloat(document.getElementById('lat').value);
+    var y = parseFloat(document.getElementById('long').value);
+    var i = 0;
+    while (i<nbh.length) {
+        if (x >= nbh[i].min_lat && x <=nbh[i].max_lat
+          && y >= nbh[i].min_long && y <= nbh[i].max_long){
+          nbrh = nbh[i].neighbourhood;
         }
+        i++;
     }
-    return loc
-  }
 
-function varCheck(x, y){
-  var s;
-  if (x>y){
-    s = "test";
-  }
-  console.log(s);
-}
+    var avg = 0;
+    /* Calculate average price of each property type for each neighbourhood */
+    for (var j=0; j<prop.length;j++){
+        for (var k=0; k<data.length;k++){
+          var a = data[k].neighbourhood;
+          var b = data[k].type;
+          var d = prop[j].type;
+          if (data[k].neighbourhood == nbrh && data[k].type == prop[j].type){
+            avg += +data[k].price;
+            prop[j].number++;
+          }
+        }
+      if (prop[j].number == 0){
+        var pType = document.getElementById('prop' + (j+1));
+        var price = document.getElementById('price' + (j+1));
+        pType.innerText = prop[j].type;
+        price.innerText = "N/A";
 
-console.log(s(data, lat, long));
+        pType = document.getElementById('prop2_' + (j+1));
+        price = document.getElementById('price2_' + (j+1));
+        pType.innerText = prop[j].type;
+        price.innerText = "N/A";
+      } else {
+        avg /= prop[j].number;
+        avg = avg.toFixed(2);
+        var pType = document.getElementById('prop' + (j+1));
+        var price = document.getElementById('price' + (j+1));
+        pType.innerText = prop[j].type;
+        price.innerText = '$' + avg;
+
+        pType = document.getElementById('prop2_' + (j+1));
+        price = document.getElementById('price2_' + (j+1));
+        pType.innerText = prop[j].type;
+        avg = (avg * 7).toFixed(2);
+        price.innerText = '$' + avg;
+      }
+      avg = 0;
+      prop[j].number = 0;
+    }
+  }
